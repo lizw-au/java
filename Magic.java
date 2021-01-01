9af1ec44d084babbb13e6697d3bf01ec51a53567
lizw-au <55069780+lizw-au@users.noreply.github.com>
2021-01-01 16:09:52 +1100

    Update Magic.java
    Added link to project task

--- Magic.java
+++ Magic.java
@@ -1,6 +1,7 @@
 public class Magic {
 	public static void main(String[] args) {
-    /*A little game of math magic - regardless of myNumber value, you'll get the same answer in stepSix. Magic!*/
+    /*A little game of math magic - regardless of myNumber value, you'll get the same answer in stepSix. Magic!
+    https://www.codecademy.com/courses/learn-java/projects/math-magic*/
     
     //this is referred to as the original number
 		int myNumber = 100;
