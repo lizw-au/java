4c71095668e26620ae12cddaa1cbb3e9311fa8b4
lizw-au <55069780+lizw-au@users.noreply.github.com>
2021-01-01 16:05:10 +1100

    Update Magic.java

--- Magic.java
+++ Magic.java
@@ -3,6 +3,14 @@ public class Magic {
     //this is referred to as the original number
 		int myNumber = 1;
 
+    int stepOne = myNumber * myNumber;
+    int stepTwo = stepOne + myNumber;
+    int stepThree = stepTwo / myNumber;
+    int stepFour = stepThree + 17;
+    int stepFive = stepFour - myNumber;
+    int stepSix = stepFive / 6;
 
+    System.out.println(StepSix);
+    
 	}
 }
