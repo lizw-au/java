249cc23d5efc1e9b74ddcdb06398bb298652aaa1
lizw-au <55069780+lizw-au@users.noreply.github.com>
2021-01-02 10:55:16 +1100

    Remove code that was refactored, update comments

--- Magic.java
+++ Magic.java
@@ -1,40 +1,21 @@
 public class Magic {
 	public static void main(String[] args) {
-    /*A little game of math magic - regardless of myNumber value, you'll get the same answer in stepSix. Magic!*/
+    /*A little game of math magic - regardless of myNumber value, you'll get the same answer as magic number! 
+    Magic!*/
     
-    //this is referred to as the original number
-		/*int myNumber = 100;
-
-    int stepOne = myNumber * myNumber;
-    int stepTwo = stepOne + myNumber;
-    int stepThree = stepTwo / myNumber;
-    int stepFour = stepThree + 17;
-    int stepFive = stepFour - myNumber;
-    int stepSix = stepFive / 6;
-
-    System.out.println(stepSix);
-    */
-
-    //refactor to only use two variables - my number and magic number... 
+  //Set this number to anything, and the magic number will still be the same. 
   int myNumber = 11111;
-  //Step1
+
   int magicNumber = myNumber * myNumber;
-  
-  //Step2
+
   magicNumber = magicNumber + myNumber;
-
-  //Step3
+  
   magicNumber = magicNumber / myNumber;
-
-  //Step4
   magicNumber = magicNumber + 17;
-
-  //Step5
   magicNumber = magicNumber - myNumber;
-
-  //Step6
   magicNumber = magicNumber / 6;
 
+  //print the magic number!			
   System.out.println(magicNumber);
 
     
